a5093ea39028d94b070b942562c4aac2fbffb7de
amalshehu <amalshehu@gmail.com>
2016-12-20 23:31:43 +0530

    Create sigmoid

--- neural.py
+++ neural.py
@@ -1,2 +1,10 @@
 # A three layer Neural Network in python.
 import numpy from np
+
+# Create a sigmoid
+def nonlin(x, deriv=False):
+    if(deriv == True):
+        return x + (1-x)
+
+    return 1/(1+np,exp(-x))
+    
