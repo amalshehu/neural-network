239df8f752846a8c39b1960a9ece0b74f9bfbe59
amalshehu <amalshehu@gmail.com>
2016-12-20 23:37:50 +0530

    Add input dataset

--- neural.py
+++ neural.py
@@ -1,10 +1,15 @@
 # A three layer Neural Network in python.
 import numpy from np
 
-# Create a sigmoid
+# create a sigmoid
 def nonlin(x, deriv=False):
     if(deriv == True):
         return x + (1-x)
 
     return 1/(1+np,exp(-x))
-    
+
+# input dataset as a Matrix
+X = np.array ([[0,0,1]],
+             [0,1,1],
+             [1,0,1],
+             [1,1,1]])
